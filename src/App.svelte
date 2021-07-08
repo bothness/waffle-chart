0fb40203adb59a87faaeeb7c88f295648ae0972a
Ahmad Barclay <abarclay.ons@gmail.com>
2021-07-08 10:38:57 +0100

    intital commit

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+	import data from "./data";
+	import GridChart from "./GridChart.svelte";
+</script>
+
+Rectangular waffle chart (25 X 4)
+<div class="wide">
+	<GridChart {data} width={4}/>
+</div>
+
+Rectangular waffle chart (20 X 5)
+<div class="narrow">
+	<GridChart {data} width={5} height={100}/>
+</div>
+
+Square waffle chart, horizontal bands
+<div class="square">
+	<GridChart {data} vertical={false} width={10} height={200}/>
+</div>
+
+As above, with circular cells
+<div class="square">
+	<GridChart {data} vertical={false} width={10} height={200} round={true}/>
+</div>
+
+<style>
+	.wide {
+		width: 500px;
+		margin-bottom: 20px;
+	}
+	.narrow {
+		width: 400px;
+		margin-bottom: 20px;
+	}
+	.square {
+		width: 200px;
+		margin-bottom: 20px;
+	}
+	.legend {
+		width: 400px;
+		margin-top: 3px;
+		margin-bottom: 20px;
+	}
+</style>
---
+++ src/data.js
@@ -0,0 +1,8 @@
+export default [
+	{x: "one", y: 50},
+	{x: "two", y: 30},
+	{x: "three", y: 10},
+	{x: "four", y: 5},
+	{x: "five", y: 3},
+	{x: "six", y: 2}
+];
